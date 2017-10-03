d06b761d5c7ca1d0b2ddb469c8dfc90cadd92dd2
jaLeague <jackie.arseneau@jointheleague.org>
2017-10-03 15:15:57 -0700

    Create RobotColorChooser.java

---
+++ src/elseif/RobotColorChooser.java
@@ -0,0 +1,27 @@
+
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+package elseif;
+
+import javax.swing.JOptionPane;
+import java.awt.Color;
+
+public class RobotColorChooser {
+	public static void main(String[] args) {
+
+		//3. ask the user what color they would like the robot to draw
+		
+		//4. use an if/else statement to set the pen color that the user requested
+
+        //5. if the user doesn’t enter anything, choose a random color
+
+        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
+		
+		//2. set the pen width to 10
+		
+	    //1. make the robot draw a shape (this will take more than one line of code)
+
+
+	}
+}
